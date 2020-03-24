dc10d04899b40cc6b4e608da5418c2349028e0fd
Harsh <harsh.26020@gmail.com>
2020-03-24 23:19:01 +0530

    Added requirements.txt

--- blockchain.py
+++ blockchain.py
@@ -99,13 +99,12 @@ class Blockchain(object):
 
 		# Verify the chains from all nodes in the network
 		for node in neighbours:
-			print(node)
 			response = requests.get(f'http://{node}/chain')
 
 			if response.status_code == 200:
 				length = response.json()['length']
 				chain = response.json()['chain']
-				print(length, chain)
+
 				# Check if current chain is longer and valid 
 				if length > max_length and self.valid_chain(chain):
 					max_length = length
