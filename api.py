8a5b0ec1f5108a5f0611705b1a3e834eee6d59fa
Harsh <harsh.26020@gmail.com>
2020-03-24 18:46:31 +0530

    Added api

---
+++ api.py
@@ -0,0 +1,71 @@
+import hashlib
+import json
+from textwrap import dedent
+from time import time
+from blockchain import Blockchain
+from uuid import uuid4
+from flask import Flask, jsonify, request
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+ 	# We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+  
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+	values = request.get_json()
+    
+    # Check if all required fields are available in posted data
+    required = ['sender', 'recipient', 'amount']
+    for r in required:
+    	if r not in values:
+    		return 'Missing values', 400
+
+    index = blockchain.new_transaction(values['sender'], 
+    	values['recipient'], values['amount'])
+
+    response = {'message' : f'Transaction will be added to Block {index}'}
+    return jsonify(response), 200
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
